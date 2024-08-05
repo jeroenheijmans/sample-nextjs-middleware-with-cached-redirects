6fac8f8a1939d094846b0062a42a63c30140cc55
Jeroen Heijmans <github@jeroenheijmans.nl>
2024-08-05 21:15:36 +0200

    Initialize NextJS app with Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  extends: ["next/core-web-vitals", "plugin:prettier/recommended"],
+  plugins: ["simple-import-sort", "prettier"],
+  rules: {
+    "simple-import-sort/imports": [
+      "warn",
+      {
+        groups: [
+          // Packages `react` related packages come first.
+          ["^react", "^@?\\w"],
+          // Internal packages.
+          ["^(@|components)(/.*|$)"],
+          // Side effect imports.
+          ["^\\u0000"],
+          // Parent imports. Put `..` last.
+          ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+          // Other relative imports. Put same-folder imports and `.` last.
+          ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+          // Style imports.
+          ["^.+\\.?(css)$"],
+        ],
+      },
+    ],
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+  },
+};
